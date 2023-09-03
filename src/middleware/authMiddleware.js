260fa15dec5d5dabc0bbc54556c946c8c918d951
Parzival-05 <parzivalwasd@gmail.com>
2023-09-03 22:30:05 +0300

    Fix auth route

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 const JWT = require('jsonwebtoken')
+const { User } = require('../models/index')
 
-module.exports = function (req, res, next) {
+module.exports = async (req, res, next) => {
     if (req.method === "OPTIONS") {
         next()
     }
@@ -10,11 +11,10 @@ module.exports = function (req, res, next) {
             return res.status(401).json({ message: "Не авторизован" })
         }
         const decoded = JWT.verify(token, process.env.SECRET_KEY)
-        req.user = decoded
+        req.user = await User.findOne({ where: { email: decoded.email } })
         next()
     }
     catch (e) {
-        console.log(e)
         res.status(401).json({ message: "Не авторизован" })
     }
 }
